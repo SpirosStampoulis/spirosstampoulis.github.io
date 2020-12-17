256bd20116d930dcb7f4569393ed5ef0cea7aa66
StampoulisSpiros <Darkdream3r>
2020-12-17 18:58:54 +0100

    new relic

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
-
   constructor(props){
     super(props);
     this.state = {
